b736e629b6af0c5512ae29ce97ebebad627a06af
khai.truong <khai.truong@trustingsocial.com>
2022-06-27 10:32:43 +0700

    Remove duplicate

--- utils/src/services/product.js
+++
@@ -1,9 +0,0 @@
-import { BehaviorSubject } from "rxjs";
-
-const productSubject = new BehaviorSubject(null);
-
-export const update = (product) => {
-  productSubject.next(product);
-};
-
-export const subscribe = (listener) => productSubject.subscribe(listener);
